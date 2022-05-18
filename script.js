8bfa1dca3bfbe03f7ae09af662bb0fc61aa16ae0
GunDumMan <55555topkung@gmail.com>
2022-05-18 15:06:17 +0700

    mouse x y in class

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+document.getElementById('ok').addEventListener('click', function (e) {
+    alert('ok is clicked');
+    console.log(e);
+})
 
-var newText = 'newText';
-document.getElementById('ok').innerHTML = newText;
-document.getElementById('no').innerHTML = '<b><i>TOP</i></b>';   
+document.getElementById('cancel').addEventListener('mousemove', function (e) {
+    console.log('x : ' + e.screenX + ' y: ' + e.screenY);
+})
+
+let elems = document.getElementsByClassName('flex-item');
+console.log(elems);
+
+for (elem of elems) {
+    elem.addEventListener('mousemove', function (e) {
+        console.log('x: ' + e.screenX + ' y: ' + e.screenY);
+    })
+
+}
